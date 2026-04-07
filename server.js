aefc63b07b42c00292bc978f78dc3ad1c4d36092
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-04-07 11:48:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.use(express.json());
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const response = await axios.post("http://localhost:11434/api/generate", {
+      model: "llama3",
+      prompt: message,
+      stream: false
+    });
+
+    res.json({ reply: response.data.response });
+  } catch (err) {
+    res.status(500).send("Error");
+  }
+});
+
+app.listen(3000, () => console.log("API running on port 3000"));
