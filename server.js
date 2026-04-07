60d29a988e55b8c36f7495d772ec0921d407f5dc
Pavan Raghuwanshi <pavanraghuwanshi77@gmail.com>
2026-04-07 12:58:18 +0530

    sift in mistral

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.post("/chat", async (req, res) => {
 
   try {
     const response = await axios.post("http://localhost:11434/api/generate", {
-      model: "llama3",
+     //  model: "llama3",
+     model: "mistral",
       prompt: message,
       stream: false
     });
